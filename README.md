6357fbb4c1d4369142e9b80a8033c1ac671e4935
iMGamal <mohamed.gamal126027@gmail.com>
2024-05-21 00:14:37 +0000

    1

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
-# This is a model for web application that uses command line interpreter.
-# It starts by executing the program
-# It is used through passing arguments that follow syntax documented in program
-# For example, passing a group of numbers into a program that provides their sum
+# This is a command interpreter.
+# It takes input and process it.
--- console.py
+++ console.py
@@ -1,21 +1,14 @@
-"""Module to display current date and time."""
-from datetime import datetime
+"""This is a module for our class Step."""
 
 
-class Fun:
-    """Class for practice."""
+class Step:
+    """This is our main class."""
 
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
+    def __init__(self, height, weight):
+        """CLASS constructor."""
+        self.height = height
+        self.weight = weight
 
-    def __str__(self):
-        return f"Hey, {self.a}. {self.b} is coming"
-
-mod = Fun(7, 3)
-print(mod)
-current = datetime.now()
-now = current.isoformat()
-print(current)
-print(now)
-
+    def area(self):
+        """RETURN string."""
+        return f"area = {self.height} * {self.weight}"
