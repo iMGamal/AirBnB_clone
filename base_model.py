05b94cacb0dd0a479c2bc9cb3c1c434c7de9cd09
imgamal <mohamed.gamal126027@gmail.com>
2024-07-11 12:36:06 -0700

    basemodel

---
+++ base_model.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Modules for the base model."""
+
+
+import uuid
+import datetime
+
+class BaseModel:
+    """Class of the base model."""
+
+    def __init__(self):
+        """Class constructor."""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Returns string representation."""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """Saves updates of upadates_at attribute."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns dictionary representation."""
+        base_dict = self.__dict__.copy()
+        base_dict["__class__"] = self.__class__.__name__
+        base_dict["created_at"] = self.created_at.datetime.isoformat()
+        base_dict["updated_at"] = self.updated_at.datetime.isoformat()
+        return base_dict
