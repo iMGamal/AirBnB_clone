373acde1a148f62904cbfa5ac47f830dff905107
imgamal <mohamed.gamal126027@gmail.com>
2024-07-11 15:12:40 -0700

    stall

---
+++ tests/test_base_models.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Modules for testing the base model."""
+
+
+import unittest
+from models.base_model import BaseModel
+import uuid
+import datetime
+
+
+class Testing(unittest.TestCase):
+    """Class for testing the base model."""
+
+
