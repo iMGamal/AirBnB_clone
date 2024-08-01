c76e48d6f26a653fc4a74e7457e1171111daa7cf
iMGamal <mohamed.gamal126027@gmail.com>
2024-08-01 12:38:06 -0700

    Updating BaseModel to handle kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,17 @@ import datetime
 
 class BaseModel:
     """Class of project."""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Class constructor."""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ('created_at', 'updated_at'):
+                        setattr(self, key, datetime.datetime.fromisoformat(value))
+
 
     def __str__(self):
         """Returns string representation."""
