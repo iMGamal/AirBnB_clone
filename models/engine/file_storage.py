2f609c47f0f9be4a8b91978ad8ac29bde3b8bc28
iMGamal <mohamed.gamal126027@gmail.com>
2024-08-01 16:58:31 -0700

    Handling JSON

---
+++ models/engine/file_storage.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Storing saved models."""
+from base_model import BaseModel
+import json
+import os
+
+
+class FileStorage:
+    """Class for data storage."""
+    __file_path = "file_path"
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary __objects."""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id"""
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """Serializes __objects to the JSON file (path: __file_path)"""
+        serialized_objects = {}
+        for key, obj in FileStorage.__objects.items():
+            serialized_objects[key] = obj.to_dict()
+
+    def reload(self):
+        """Deserializes the JSON file to __objects"""
+        if os.path.exists(FileStorage.__file_path):
+             with open(FileStorage.__file_path, 'r') as f:
+                obj_dict = json.load(f)
+                for key, value in obj_dict.items():
+                    class_name, obj_id = key.split('.')
+                    obj = eval(class_name)(**value)
+                    FileStorage.__objects[key] = obj
