26381ab3a54f59d8f9676ff67d035d17970e8ece
iMGamal <mohamed.gamal126027@gmail.com>
2024-08-01 11:03:49 -0700

    Creating a BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,42 +1,29 @@
 #!/usr/bin/python3
-"""Modules for the base model."""
-
-
-from uuid import uuid4
-from datetime import datetime
+"""Gather data for each model."""
+import uuid
+import datetime
 
 
 class BaseModel:
-    """Class of the base model."""
-
-    def __init__(self, *args, **kwargs):
+    """Class of project."""
+    def __init__(self):
         """Class constructor."""
-        if not kwargs:
-            self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
-            for i, j in kwargs.items():
-                if i != "__class__":
-                    if i == "created_at":
-                        self.created_at = datetime.fromisoformat(j)
-                    elif i == "updated_at":
-                        self.updated_at = datetime.fromisoformat(j)
-                    else:
-                        setattr(self, i, j)
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
-        """Return string representation."""
+        """Returns string representation."""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """Save updates of upadated_at attribute."""
-        self.updated_at = datetime.now()
+        """Updates updated_at attribute."""
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
-        """Return dictionary representation."""
-        base_dict = self.__dict__.copy()
-        base_dict["__class__"] = self.__class__.__name__
-        base_dict["created_at"] = self.created_at.isoformat()
-        base_dict["updated_at"] = self.updated_at.isoformat()
-        return base_dict
+        """Converts input into dictionary."""
+        dictionary = self.__dict__.copy()
+        dictionary['__class__'] = self.__class__.__name__
+        dictionary['created_at'] = dictionary['created_at'].isoformat()
+        dictionary['updated_at'] = dictionary['updated_at'].isoformat()
+        return dictionary
